db2cc0d306c43ae8b4c243fa2b283c726a9fe054
Cinux-Chosan <179817004@qq.com>
2018-01-19 00:08:29 +0800

    Update vue-loader.conf.js

--- app/build/vue-loader.conf.js
+++ app/build/vue-loader.conf.js
@@ -9,9 +9,7 @@ const sourceMapEnabled = isProduction
 module.exports = {
   loaders: utils.cssLoaders({
     sourceMap: sourceMapEnabled,
-    extract: isProduction,
-    scss: 'vue-style-loader!css-loader!sass-loader',
-    sass: 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
+    extract: isProduction
   }),
   cssSourceMap: sourceMapEnabled,
   cacheBusting: config.dev.cacheBusting,
