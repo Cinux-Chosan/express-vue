7f441592c5ddf68f14a97a956fac2b8feb52adf5
Chosan <179817004@qq.com>
2018-03-18 22:23:26 +0800

    添加sass, sass-module 等支持

--- app-ember/app/pods/application/adapter.js
+++ app-ember/app/pods/application/adapter.js
@@ -9,8 +9,8 @@ export default DS.JSONAPIAdapter.extend({
     host: computed(() => location.hostname.includes('localhost') ? '//localhost:3000' : '/'),
 
     buildURL(modelName, id, snapshot, requestType = '') {
-        if (requestType.toLowerCase().includes('record')) {
-            inflector.uncountable(modelName);
+        if (requestType.toLowerCase().includes('record')) { // 像 findRecord 这样查找单条记录的方法，就阻止 REST 将它转换成复数， 只是个人习惯
+            inflector.uncountable(modelName); 
         }
         return this._super(...arguments);
     }
--- app-ember/app/pods/application/serializer.js
+++ app-ember/app/pods/application/serializer.js
@@ -3,7 +3,6 @@ import DS from 'ember-data';
 export default DS.JSONSerializer.extend({
     primaryKey: '_id',
     serialize() {
-        debugger
     },
     normalizeResponse(store, primaryModelClass, payload, id, requestType) {
         if (payload.state) {
