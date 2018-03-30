61ca76bab2e443917635a3d6959d6cc24077bdf3
chosan <179817004@qq.com>
2018-03-30 19:06:16 +0800

    ..完成分类管理

--- app-ember/app/services/check-login.js
+++ app-ember/app/services/check-login.js
@@ -6,14 +6,14 @@ export default Service.extend({
   isShowingModal: '',
   userName: '',
   userPwd: '',
-  isLogged: false,
+  isLoggedIn: false,
   async login() {
     let { userName: name, userPwd: pwd } = this.getProperties(['userName', 'userPwd']);
     let result = await getJson('api/login', { name, pwd }, 'post');
     if (result.meta.state) {
       tip('登陆成功!');
       this.set('isShowingModal', '');
-      this.set('isLogged', true);
+      this.set('isLoggedIn', true);
       this.set('userName', '');
       this.set('userPwd', '');
     }
