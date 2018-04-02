a4323a822d18ccb9a215eaabd2b8cf37c8c0a495
Chosan <179817004@qq.com>
2018-04-02 23:47:06 +0800

    ..

--- app-ember/app/pods/edit/controller.js
+++ app-ember/app/pods/edit/controller.js
@@ -15,7 +15,7 @@ export default Controller.extend({
   actions: {
     submit() {
       let { model, post_type, editor } = this.getProperties(['model', 'post_type', 'editor']);
-      model.set('postType', model.postType || POSTTYPE[+post_type]);
+      model.set('postType', model.get('postType') || POSTTYPE[+post_type]);
       model.set('content', editor.getContent());
       this.get('model').save();
     },
