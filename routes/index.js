5fbe0779e7fe63fc6c3822bf8cfd363c1df806ff
chosan <179817004@qq.com>
2018-04-04 18:46:19 +0800

    ..https

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
+var util = require('util');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // res.render('index', { title: 'Express' });
+  return res.end(util.inspect(req));
   if (req.hostname.startsWith('vue.')) {
     res.sendFile(path.resolve('./app-vue/dist/index.html'));
   } else {
