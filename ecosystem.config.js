d49151108362286640e9daf1879020bd4bf20840
chosan <179817004@qq.com>
2018-08-27 11:08:54 +0800

    ..

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,25 +1,28 @@
 module.exports = {
-  apps : [{
-    name      : 'post',
-    script    : 'bin/www',
-    env: {
-      NODE_ENV: 'development'
-    },
-    env_production : {
-      NODE_ENV: 'production'
+  apps: [
+    {
+      name: "post",
+      script: "bin/www",
+      env: {
+        NODE_ENV: "development"
+      },
+      env_production: {
+        NODE_ENV: "production"
+      }
     }
-  }],
+  ],
 
-  deploy : {
-    production : {
-      host : 'chosan.cn',
-      user: 'root',
-      ref  : 'origin/master',
+  deploy: {
+    production: {
+      host: "chosan.cn",
+      user: "root",
+      ref: "origin/master",
       ssh_options: "StrictHostKeyChecking=no",
-      repo : 'git@github.com:Cinux-Chosan/express-vue.git',
-      path : '/tmp',
-      'post-setup': 'npm install',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+      repo: "git@github.com:Cinux-Chosan/express-vue.git",
+      path: "/zhangjianjun",
+      "post-setup": "npm i",
+      "post-deploy":
+        "yarn && cd app-ember && yarn && ember b --prod && cd .. && node upload-statics.js && pm2 reload ecosystem.config.js --env production"
     }
   }
 };
