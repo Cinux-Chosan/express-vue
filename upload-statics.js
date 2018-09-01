de58990eee4ae5029b0988997fdc2522fa91329d
Your Name <you@example.com>
2018-09-01 15:20:30 +0800

    preDeployLocalHook

--- upload-statics.js
+++ upload-statics.js
@@ -2,21 +2,40 @@ let { colUpload } = require('./lib/utils/cos');
 let glob = require("glob");
 let assert = require('assert');
 let path = require('path');
+const { createReadStream, createWriteStream } = require('fs');
+const zlib = require('zlib');
+
 
 glob('app-ember/dist/+(assets|images)/**/*.*', { absolute: true }, (err, files) => {
   assert.equal(null, err);
   let count = 0;
-  files.forEach(el => {
+  files.forEach(FilePath => {
+    let options = {
+      Key: '/app-ember/' + path.relative('app-ember/dist', FilePath), 
+      FilePath
+    };
 
-    // let basename = path.basename(el);
-    // console.log(basename, el);
-    // console.log('/app-ember/' + path.relative('app-ember/dist', el));
-
-    colUpload('/app-ember/' + path.relative('app-ember/dist', el), el, (err, data) => {
-      assert.equal(null, err);
-      if (++count === files.length) {
-        console.log('静态文件上传完成');
-      }
-    });
+    if (FilePath.match(/.(js|css)$/)) {
+      let ws = createWriteStream(FilePath);
+      ws.on('finish', () => {
+        console.log(`文件\t${FilePath}\t压缩完成`);
+        colUpload(options, err => {
+          assert.equal(null, err);
+          if (++count === files.length) {
+            console.log('静态文件上传完成');
+          }
+        });
+      })
+      createReadStream(FilePath).pipe(zlib.createGzip()).pipe(ws);
+      options.contentEncoding = 'gzip';
+    } else {
+      colUpload(options, err => {
+        assert.equal(null, err);
+        if (++count === files.length) {
+          console.log('静态文件上传完成');
+        }
+      });
+    }
   })
 })
+
